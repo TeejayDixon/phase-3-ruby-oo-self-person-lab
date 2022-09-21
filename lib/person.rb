ab1a56d99a3e4be6cb0ea42ac632ceabfca9ff8e
Teejay Dixon <tahjgdixon@gmail.com>
2022-09-21 16:01:35 -0700

    commit tree 33c65d4a7b072134d803cddfc6362a566b4bfa19

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,56 @@
 # your code goes here
+
+class Person
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25 
+    @happiness = 8 
+    @hygiene = 8
+  end
+
+   def happiness=(value)
+    if value > 10
+      @happiness = 10
+    elsif value < 0 
+      @happiness = 0
+    else 
+      @happiness = value 
+    end
+  end
+
+    def hygiene=(value)
+      if value > 10
+        @hygiene = 10
+      elsif value < 0 
+        @hygiene = 0
+      else 
+        @hygiene = value
+      end 
+    end
+
+    def happy?
+      @happiness > 7
+    end
+
+    def clean?
+      @hygiene > 7
+    end
+
+    def get_paid(amount) 
+      @bank_account += amount
+    return "all about the benjamins"
+    end
+      
+   
+  
+
+
+
+   
+    
+end
+
+
